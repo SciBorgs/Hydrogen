1b7890b689967553610ea7f1bc65b5544c7b27db
ethan jones <jonese>
2024-01-03 17:18:37 -0500

    Shooter basic template

--- src/main/java/org/sciborgs1155/robot/Shooter/Shooter.java
+++ src/main/java/org/sciborgs1155/robot/Shooter/Shooter.java
@@ -1,5 +1,38 @@
 package org.sciborgs1155.robot.Shooter;
 
-public class Shooter {
-    
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Shooter extends SubsystemBase {
+    CANSparkMax motor = new CANSparkMax(ShooterConstants.deviceID, MotorType.kBrushless);
+    RelativeEncoder encoder = motor.getEncoder();
+    private final PIDController pidController = new PIDController(ShooterConstants.kp, ShooterConstants.ki, ShooterConstants.kd);
+
+    public Shooter() {
+        setDefaultCommand(
+        runOnce(
+                () -> {
+                  motor.disable();
+                  //m_feederMotor.disable();
+                })
+            .andThen(run(() -> {}))
+            .withName("Idle"));
+  }
+
+  public Command shootCommand(double setpointRPS) {
+            // Run the shooter flywheel at the desired setpoint using feedforward and feedback
+            run(
+                () ->
+                    motor.set(
+                            pidController.calculate(
+                                encoder.getRate(), setpointRPS)))
+            // Wait until the shooter has reached the setpoint, and then run the feeder
+        .withName("Shoot");
+  }
 }
+
--- src/main/java/org/sciborgs1155/robot/Shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/Shooter/ShooterConstants.java
@@ -1,5 +1,8 @@
 package org.sciborgs1155.robot.Shooter;
 
 public class ShooterConstants {
-    
+    static int deviceID = 0;
+    static int ki = 0;
+    static double kp = 0.03;
+    static int kd = 0;
 }
