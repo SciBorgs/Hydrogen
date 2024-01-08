d118b4b5827c0109f70de3c1c21c0006f4c6ef45
HenryLi-0 <bxsci8266@gmail.com>
2024-01-08 17:52:07 -0500

    aa

--- src/main/java/org/sciborgs1155/robot/Shooter/Shooter.java
+++ src/main/java/org/sciborgs1155/robot/Shooter/Shooter.java
@@ -1,8 +1,5 @@
 package org.sciborgs1155.robot.Shooter;
 
-import org.sciborgs1155.robot.Constants;
-import org.sciborgs1155.robot.Robot;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -15,7 +12,6 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Shooter extends SubsystemBase implements Logged {
@@ -24,8 +20,10 @@ public class Shooter extends SubsystemBase implements Logged {
     @LogBoth
     private final PIDController pidController = new PIDController(ShooterConstants.kp, ShooterConstants.ki, ShooterConstants.kd);
     private final SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(ShooterConstants.kSVolts, ShooterConstants.kVVoltSecondsPerRotation);
-    @LogBoth
     private final FlywheelSim flyWheelSim = new FlywheelSim(LinearSystemId.identifyVelocitySystem(ShooterConstants.kVVoltSecondsPerRotation, 1), DCMotor.getNEO(1), 1);
+    
+    @LogBoth
+    private double lastVelocity;
 
     public Shooter() {
         setDefaultCommand(
@@ -41,28 +39,22 @@ public class Shooter extends SubsystemBase implements Logged {
             // Run the shooter flywheel at the desired setpoint using feedforward and feedback
             return run(
                 () ->
-                    motor.setVoltage(
-                            pidController.calculate(getVelocity(), setpointRPS)
+                    motor.set(
+                            pidController.calculate(encoder.getVelocity(), setpointRPS)
                                 + feedForward.calculate(setpointRPS)))
             // Wait until the shooter has reached the setpoint, and then run the feeder
         .withName("Shoot");
   }
 
-  @LogBoth
-  public double getVelocity() {
-    return Robot.isReal() ? encoder.getVelocity() : flyWheelSim.getAngularVelocityRadPerSec();
+  public double getSpeed(double distance) {
+    //auto function, siggy told me to
+    //random number
+    return 1;
   }
 
   @Override
   public void periodic() {
-    
-  }
-
-  @Override
-  public void simulationPeriodic() {
-      flyWheelSim.setInputVoltage(motor.getAppliedOutput());
-      flyWheelSim.update(Constants.PERIOD);
-      System.out.println(motor.getAppliedOutput());
+      lastVelocity = encoder.getVelocity();
   }
 }
 
