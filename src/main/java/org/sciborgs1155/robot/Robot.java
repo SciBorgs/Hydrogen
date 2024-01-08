4e8806f58acab85ac99579af4302fb0a176ed416
ethan jones <jonese>
2024-01-08 16:24:19 -0500

    git stuff

--- src/main/java/org/sciborgs1155/robot/Robot.java
+++ src/main/java/org/sciborgs1155/robot/Robot.java
@@ -66,7 +66,7 @@ public class Robot extends CommandRobot implements Logged, Fallible {
   /** Configures trigger -> command bindings */
   private void configureBindings() {
     autonomous().whileTrue(new ProxyCommand(autos::get));
-    operator.x().onTrue(shooter.shootCommand(1));
+    operator.x().whileTrue(shooter.shootCommand(1));
   }
 
   @Override
