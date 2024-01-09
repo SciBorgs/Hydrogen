bd03841f14368f8b93c09bb2c0ce9bfa4c1a2203
HenryLi-0 <bxsci8266@gmail.com>
2024-01-09 17:51:41 -0500

    pid constant changes

--- src/main/java/org/sciborgs1155/robot/Shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/Shooter/ShooterConstants.java
@@ -3,8 +3,8 @@ package org.sciborgs1155.robot.Shooter;
 public class ShooterConstants {
     static int deviceID = 0;
     static double ki = 0.0; // i heard advice to keep the PID I at 0
-    static double kp = 0.3;
-    static double kd = 0.01; //i tried
+    static double kp = 0.8;
+    static double kd = 0.15; //i tried
     public static final double kSVolts = 0.05;
     public static final double kShooterFreeRPS = 5300;
     public static final double kVVoltSecondsPerRotation =
