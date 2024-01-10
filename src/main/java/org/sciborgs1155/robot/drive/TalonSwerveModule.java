dd4e8e4a7eb3d363b5a8a825e62bedc7542d9c68
Yxhej <wui@bxscience.edu>
2024-01-10 17:57:48 -0500

    more drive work

--- src/main/java/org/sciborgs1155/robot/drive/TalonSwerveModule.java
+++ src/main/java/org/sciborgs1155/robot/drive/TalonSwerveModule.java
@@ -4,20 +4,30 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import org.sciborgs1155.robot.drive.DriveConstants.SwerveModule.Driving;
 
 public class TalonSwerveModule implements ModuleIO {
   private final TalonFX driveMotor;
+
+  // change to sparkmax (neo 550)
   private final TalonFX turnMotor;
 
+  private final SimpleMotorFeedforward turnFeedForward =
+      new SimpleMotorFeedforward(Driving.FF.S, Driving.FF.V, Driving.FF.A);
+  private final SimpleMotorFeedforward driveFeedForward =
+      new SimpleMotorFeedforward(Driving.FF.S, Driving.FF.V, Driving.FF.A);
+
   private SwerveModuleState setpoint = new SwerveModuleState();
-  ;
 
   public TalonSwerveModule(int drivePort, int turnPort) {
     driveMotor = new TalonFX(drivePort);
     turnMotor = new TalonFX(turnPort);
+
+    driveMotor.getConfigurator().apply(new TalonFXConfiguration());
 
     configureDrive(driveMotor.getConfigurator());
     configureTurn(turnMotor.getConfigurator());
@@ -25,10 +35,8 @@ public class TalonSwerveModule implements ModuleIO {
     resetEncoders();
 
     driveMotor.getPosition().setUpdateFrequency(100);
-    turnMotor.getPosition().setUpdateFrequency(100);
 
     driveMotor.getVelocity().setUpdateFrequency(100);
-    turnMotor.getVelocity().setUpdateFrequency(100);
   }
 
   // resolve magic numbers
@@ -61,7 +69,19 @@ public class TalonSwerveModule implements ModuleIO {
   }
 
   @Override
-  public void setDesiredState(SwerveModuleState desiredState) {}
+  public void setDesiredState(SwerveModuleState desiredState) {
+    SwerveModuleState correctedDesiredState = new SwerveModuleState();
+    correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
+    correctedDesiredState.angle = desiredState.angle.plus(angularOffset);
+    // Optimize the reference state to avoid spinning further than 90 degrees
+    setpoint =
+        SwerveModuleState.optimize(
+            correctedDesiredState, Rotation2d.fromRadians(turnMotor.getPosition().getValue()));
+
+    double driveFF = driveFeedforward.calculate(setpoint.speedMetersPerSecond);
+    driveFeedback.setReference(setpoint.speedMetersPerSecond, ControlType.kCurrent, 0, driveFF);
+    turnFeedback.setReference(setpoint.angle.getRadians(), ControlType.kPosition);
+  }
 
   @Override
   public SwerveModuleState getDesiredState() {
