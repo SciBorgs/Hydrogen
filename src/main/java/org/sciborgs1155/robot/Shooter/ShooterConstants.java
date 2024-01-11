a9f11583a2568fb0f35488b983981994683872e7
Steven Jones <metacoderminecraft@gmail.com>
2024-01-10 20:39:10 -0500

    Updated versions

--- src/main/java/org/sciborgs1155/robot/Shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/Shooter/ShooterConstants.java
@@ -3,7 +3,7 @@ package org.sciborgs1155.robot.shooter;
 public class ShooterConstants {
   static int deviceID = 0;
   static double ki = 0.0; // i heard advice to keep the PID I at 0
-  static double kp = 1.1;
+  static double kp = 1.5;
   static double kd = 0; // i tried
   public static final double kSVolts = 0.05;
   public static final double kShooterFreeRPS = 5300;
