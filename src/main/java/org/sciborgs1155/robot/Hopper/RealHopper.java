eb9a4d7ccf926b9860b179dd6e89c713258963dd
jonahk17 <jonahkwon@jonahs-air-2.78x445.nycboe.org>
2024-01-17 17:56:42 -0500

    Updates to motor configuration

--- src/main/java/org/sciborgs1155/robot/Hopper/RealHopper.java
+++ src/main/java/org/sciborgs1155/robot/Hopper/RealHopper.java
@@ -1,12 +1,16 @@
 package org.sciborgs1155.robot.Hopper;
 
+import static org.sciborgs1155.lib.SparkUtils.Data.*;
+import static org.sciborgs1155.lib.SparkUtils.Sensor.*;
 import static org.sciborgs1155.robot.Hopper.HopperConstants.*;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import java.util.Set;
 import org.sciborgs1155.lib.FaultLogger;
+import org.sciborgs1155.lib.SparkUtils;
 
 public class RealHopper implements HopperIO {
   CANSparkMax motor = new CANSparkMax(HOPPER_PORT, MotorType.kBrushless);
@@ -16,6 +20,8 @@ public class RealHopper implements HopperIO {
     this.motor.restoreFactoryDefaults();
     this.motor.setIdleMode(IdleMode.kCoast);
     FaultLogger.register(motor);
+    SparkUtils.configureFrameStrategy(motor, Set.of(VELOCITY), Set.of(DUTY_CYCLE), false);
+    motor.burnFlash();
   }
 
   @Override
--- src/main/java/org/sciborgs1155/robot/Intake/RealIntake.java
+++ src/main/java/org/sciborgs1155/robot/Intake/RealIntake.java
@@ -1,21 +1,29 @@
 package org.sciborgs1155.robot.Intake;
 
+import static org.sciborgs1155.lib.SparkUtils.Data.*;
+import static org.sciborgs1155.lib.SparkUtils.Sensor.*;
 import static org.sciborgs1155.robot.Intake.IntakeConstants.*;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import java.util.Set;
 import org.sciborgs1155.lib.FaultLogger;
+import org.sciborgs1155.lib.SparkUtils;
 
 public class RealIntake implements IntakeIO {
   CANSparkMax motor = new CANSparkMax(INTAKE_PORT, MotorType.kBrushless);
   RelativeEncoder encoder = motor.getEncoder();
 
   public RealIntake() {
-    this.motor.restoreFactoryDefaults();
-    this.motor.setIdleMode(IdleMode.kCoast);
-    this.encoder = this.motor.getEncoder();
+    motor.restoreFactoryDefaults();
+    motor.setIdleMode(IdleMode.kCoast);
+
+    SparkUtils.configureFrameStrategy(motor, Set.of(VELOCITY), Set.of(DUTY_CYCLE), false);
+
+    motor.burnFlash();
+
     FaultLogger.register(motor);
   }
 
